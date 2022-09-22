cbb0b74488c7c1c28f2073aac28b3589173f6092
Toskyll <jans.guillope@gmail.com>
2022-09-22 14:14:56 +0200

    Updated command

--- commands/message_sympa.js
+++ commands/message_sympa.js
@@ -2,28 +2,24 @@ const { SlashCommandBuilder } = require("discord.js");
 const { random, floor } = require("mathjs");
 const { EmbedBuilder } = require("discord.js");
 
-const randomMessages = {
-  1: "Tu demandes à une machine de t'envoyer un message, c'est que tu n'as vraiment pas d'amis ...",
-  2: "Arthur le penguin t'observe",
-  3: "T'es magnifique ma chérie",
-};
+const randomMessages = [
+  "Tu demandes à une machine de t'envoyer un message, c'est que tu n'as vraiment pas d'amis ...",
+  "Arthur le penguin t'observe",
+  "T'es magnifique ma chérie",
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("message_sympa")
     .setDescription("renvoie un message sympathique"),
   async execute(interaction) {
-    function between(min, max) {
-      return floor(random() * (max - min) + min);
-    }
     const Embed = new EmbedBuilder()
-      .setTitle("Commande validée !")
       .addFields({
-        name: "Message Sympathique :",
-        value: randomMessages[between(1, Object.keys(randomMessages).length)],
+        name: "Message",
+        value: randomMessages[Math.floor(Math.random() * items.length)],
       })
       .setTimestamp()
       .setFooter({ text: `Message Author : FDP ca marche pas` });
-    await interaction.reply({ embeds: Embed });
+    await interaction.reply({ embeds: [Embed] });
   },
 };
